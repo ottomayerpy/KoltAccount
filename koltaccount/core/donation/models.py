124f0270336347db295ea1cd1470268c41fd5d9c
koltman <ottomayer57@yandex.ru>
2023-02-16 10:34:11 +0300

    ka-5. edit donation model

--- koltaccount/core/donation/models.py
+++ koltaccount/core/donation/models.py
@@ -1,18 +1,19 @@
-from django.contrib.auth.models import User
-from django.db import models
+from core.baseapp.models import BaseModel, UserModel
+from django.db.models import CASCADE, DateTimeField, ForeignKey, TextField
+from django.utils.translation import gettext_lazy as _
 
 
-class Donation(models.Model):
+class Donation(BaseModel):
     """ Пожертвования """
-    user = models.ForeignKey(
-        User, verbose_name='Пользователь', on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(
-        'Создано', auto_now=False, auto_now_add=True)
-    data = models.TextField('Данные платежа')
+    user = ForeignKey(
+        UserModel, verbose_name=_("Пользователь"), on_delete=CASCADE)
+    timestamp = DateTimeField(
+        verbose_name=_("Создано"), auto_now=False, auto_now_add=True)
+    data = TextField(verbose_name=_("Данные платежа"))
 
     def __str__(self):
         return self.user.username
 
     class Meta:
-        verbose_name = 'Пожертвование'
-        verbose_name_plural = 'Пожертвования'
+        verbose_name = "Пожертвование"
+        verbose_name_plural = "Пожертвования"

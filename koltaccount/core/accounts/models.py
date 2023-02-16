109521eacbf853816bf7cae74a8e2873828c9680
koltman <ottomayer57@yandex.ru>
2023-02-16 10:30:17 +0300

    ka-5. edit accounts model

--- koltaccount/core/accounts/models.py
+++ koltaccount/core/accounts/models.py
@@ -1,24 +1,20 @@
-from django.contrib.auth.models import User
-from django.db import models
+from core.baseapp.models import BaseModel, UserModel
+from django.db.models import CASCADE, CharField, ForeignKey
+from django.utils.translation import gettext_lazy as _
 
 
-class Account(models.Model):
+class Account(BaseModel):
     """ Аккаунты пользователя """
-    user = models.ForeignKey(
-        User, verbose_name='Пользователь', on_delete=models.CASCADE)
-    site = models.CharField('Сайт', max_length=255)
-    description = models.CharField('Описание', max_length=255)
-    login = models.CharField('Логин', max_length=255)
-    password = models.CharField('Пароль', max_length=255)
-    updated = models.DateTimeField(
-        'Обновлено', auto_now=True, auto_now_add=False)
-    timestamp = models.DateTimeField(
-        'Создано', auto_now=False, auto_now_add=True)
+    user = ForeignKey(UserModel, verbose_name=_(
+        "Пользователь"), on_delete=CASCADE)
+    site = CharField(verbose_name=_("Сайт"), max_length=255)
+    description = CharField(verbose_name=_("Описание"), max_length=255)
+    login = CharField(verbose_name=_("Логин"), max_length=255)
+    password = CharField(verbose_name=_("Пароль"), max_length=255)
 
     def __str__(self):
-        return f'{self.id} ({self.user.username})'
+        return self.user.username
 
     class Meta:
-        verbose_name = 'Аккаунт'
-        verbose_name_plural = 'Аккаунты'
-        ordering = ['-id', '-timestamp']
+        verbose_name = "Аккаунт"
+        verbose_name_plural = "Аккаунты"

83c4fe285e9d823a7f0e0f2116ba8649fb87171e
ottomayerpy <ottomayer57@yandex.ru>
2026-03-09 12:31:25 +0300

    theme button upgrade

--- koltaccount/frontend/static/js/theme.js
+++ koltaccount/frontend/static/js/theme.js
@@ -3,19 +3,23 @@ $(document).ready(function() {
     const $tablesorter = $('.tablesorter');
     const $toggle = $('#theme-toggle');
     const $icon = $toggle.find('.theme-toggle__icon');
-    const $text = $toggle.find('.theme-toggle__text');
+
+    function animateThemeToggle() {
+        $toggle.addClass('spin');
+        setTimeout(() => {
+            $toggle.removeClass('spin');
+        }, 600);
+    }
 
     function setTheme(theme) {
         if (theme === 'dark') {
             $tablesorter.removeClass('tablesorter-blue').addClass('tablesorter-dark');
             $body.addClass('dark-theme');
             $icon.text('☀️');
-            $text.text('Светлая тема');
         } else {
             $tablesorter.removeClass('tablesorter-dark').addClass('tablesorter-blue');
             $body.removeClass('dark-theme');
             $icon.text('🌙');
-            $text.text('Темная тема');
         }
         localStorage.setItem('siteTheme', theme);
     }
@@ -27,6 +31,7 @@ $(document).ready(function() {
 
     $toggle.on('click', function() {
         const isDark = $body.hasClass('dark-theme');
+        animateThemeToggle();
         setTheme(isDark ? 'light' : 'dark');
     });
 });

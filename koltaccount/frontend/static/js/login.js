2ded9fb805f3464d85d6420613d71a1345789668
ottomayerpy <ottomayer57@yandex.ru>
2026-03-24 21:44:20 +0300

    delete unused code

--- koltaccount/frontend/static/js/login.js
+++ koltaccount/frontend/static/js/login.js
@@ -6,16 +6,4 @@ $(function() {
     	password = $('#id_password');
         password.attr('type', password.attr('type') === 'password' ? 'text' : 'password');
     });
-
-    // Получаем информацию о пользователе
-
-    let system = browserInfo.os,
-    	browser = browserInfo.browser + ' ' + browserInfo.browserMajorVersion;
-
-    if (browserInfo.osVersion != 'Unknown') {
-    	system += ' ' + browserInfo.osVersion
-    }
-
-    $('#id_system').val(system);
-    $('#id_browser').val(browser);
 });
